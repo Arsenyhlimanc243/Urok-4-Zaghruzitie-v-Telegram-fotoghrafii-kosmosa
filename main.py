c64f897f6d2f3c03f616a1281d5f4e7fa6590ac8
Arsenyhlimanc243 <112560947+Arsenyhlimanc243@users.noreply.github.com>
2023-09-23 12:33:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,18 +16,6 @@ def download_picture(url, path, params=None):
         file.write(response.content)
 
 
-def fetch_spacex_last_launch():
-    url = "https://api.spacexdata.com/v5/launches/"
-    response = requests.get(url)
-    response.raise_for_status()
-    for response_link in response.json():
-        if response_link["links"]["flickr"]["original"]:
-            url_photos = response_link["links"]["flickr"]["original"]
-            for number, url_photo in enumerate(url_photos):
-                filename = f"images/spacex{number}.jpg"
-            download_picture(url_photo, filename)
-
-
 def extract_format_from_link(link):
     decoding_link = unquote(link)
     parse_link = urlparse(decoding_link)
@@ -35,45 +23,6 @@ def extract_format_from_link(link):
     format_path = os.path.splitext(fullname)
     file_name, format = format_path
     return format, file_name
-
-
-def nasa_get(images):
-    payload = {
-        "api_key": "2ov0b0AG8Q5WzcSz571mYwUK8h3WHg6WsX2J2Q1j",
-        "count": images
-    }
-    response = requests.get("https://api.nasa.gov/planetary/apod",
-                            params=payload)
-    response.raise_for_status()
-    images_json = response.json()
-    for image_json in images_json:
-        if image_json.get("media_type") == "image":
-            if image_json.get("hdurl"):
-                url_photos = image_json["hdurl"]
-            else:
-                url_photos = image_json["url"]
-            format, file_name = extract_format_from_link(url_photos)
-            path = os.path.join("images", f"{file_name}{format}")
-            download_picture(url_photos, path, params=payload)
-
-
-def EPIC_nasa(images):
-    params = {
-        "api_key": "2ov0b0AG8Q5WzcSz571mYwUK8h3WHg6WsX2J2Q1j",
-        "count": images,
-    }
-    response = requests.get("https://api.nasa.gov/EPIC/api/natural/image",
-                            params=params)
-    response.raise_for_status()
-    url_json = response.json()
-    for image_url in url_json:
-        image_urls = image_url["image"]
-        image_date = image_url["date"]
-        image_date = datetime.fromisoformat(image_date).strftime("%Y/%m/%d")
-        new_url = f"https://api.nasa.gov/EPIC/archive/natural/{image_date}/png/{image_urls}.png"
-        print(new_url)
-        path = os.path.join("images", f"{image_urls}.png")
-        download_picture(new_url, path, params)
 
 
 def main():
@@ -92,7 +41,7 @@ def main():
             filepath = os.path.join(files, image)
             with open(filepath, "rb") as f:
                 bot.send_document(chat_id=telegram_id, document=f)
-        sleep(int(str(time_repeat)))
+        sleep(time_repeat)
 
 
 if __name__ == "__main__":
