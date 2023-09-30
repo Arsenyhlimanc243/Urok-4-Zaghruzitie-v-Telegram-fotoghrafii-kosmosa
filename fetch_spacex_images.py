6781fab46fd464968978debb265d9e27c869e07a
Arsenyhlimanc243 <112560947+Arsenyhlimanc243@users.noreply.github.com>
2023-09-30 13:01:54 +0300

    Update fetch_spacex_images.py

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -3,14 +3,6 @@ import argparse
 from download_picture import download_picture
 
 
-if launch_id=None:
-     url = f"https://api.spacexdata.com/v5/launches/{launch_id}"
-else:
-     url = "https://api.spacexdata.com/v5/launches/"
-response = requests.get(url)
-response.raise_for_status()
-
-
 def fetch_spacex_last_launch():
     for response_link in response.json():
         if response_link["links"]["flickr"]["original"]:
@@ -21,12 +13,16 @@ def fetch_spacex_last_launch():
 
 
 def main():
-    parser = argparse.ArgumentParser(
-    description="Этот скрипт загружает фото от SpaceX по указанному ID запуска"
-)
-    parser.add_argument('--id', default=None, help='ID запуска, по которому загружается фото от SpaceX', dest="ID")
-    args = parser.parse_args()
-    fetch_spacex_last_launch(args.ID)
+     parser = argparse.ArgumentParser(description="Этот скрипт загружает фото от SpaceX по указанному ID запуска")
+     parser.add_argument('--id', default=None, help='ID запуска, по которому загружается фото от SpaceX', dest="ID")
+     args = parser.parse_args()
+     if launch_id=None:
+          url = f"https://api.spacexdata.com/v5/launches/{launch_id}"
+     else:
+          url = "https://api.spacexdata.com/v5/launches/"
+     response = requests.get(url)
+     response.raise_for_status()
+     fetch_spacex_last_launch(args.ID)
 
 
 if __name__ == "__main__":
