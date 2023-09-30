c8b1803c9b0af3d17b7edacdb309fa483ed036ba
Arsenyhlimanc243 <112560947+Arsenyhlimanc243@users.noreply.github.com>
2023-09-30 13:05:44 +0300

    Update fetch_spacex_images.py

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -5,8 +5,7 @@ from download_picture import download_picture
 
 def fetch_spacex_last_launch():
     for response_link in response.json():
-        if response_link["links"]["flickr"]["original"]:
-            url_photos = response_link["links"]["flickr"]["original"]
+        url_photos = response_link["links"]["flickr"]["original"]
             for number, url_photo in enumerate(url_photos):
                 filename = f"images/spacex{number}.jpg"
                 download_picture(url_photo, filename)
